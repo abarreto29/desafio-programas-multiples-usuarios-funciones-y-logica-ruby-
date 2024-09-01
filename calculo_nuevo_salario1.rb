f775d1323edbb8b26d9f99489df876f3b1bf456d
abarreto29 <106893299+abarreto29@users.noreply.github.com>
2024-09-01 14:43:13 -0400

    Update calculo_nuevo_salario1.rb
    correcion

--- calculo_nuevo_salario1.rb
+++ calculo_nuevo_salario1.rb
@@ -1,7 +1,7 @@
 # Se solicita un programa que mediante el uso de métodos permita calcular el nuevo salario de un colaborador/a en función de un porcentaje mínimo de aumento establecido.
 
 def calculo
-    sActual = ARGV[0].chomp.to_f
+    sActual = ARGV[0].to_i
     incremento = 0.10
     sNuevo = (sActual * incremento) + sActual
     puts "Su salario con incremento incluído será: #{sNuevo}"
